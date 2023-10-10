bd7ee260617bdce26fb15d403289fbf39abb14bb
muoncode <save230421@gmail.com>
2023-10-10 18:15:37 +0700

    code get subtitle from file subtitle srt

--- src/components/TranslateSubtitle/TranslateSubtitle.js
+++ src/components/TranslateSubtitle/TranslateSubtitle.js
@@ -1,15 +1,39 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import srtParser2 from "srt-parser-2";
 // import axios from "axios";
 
 import "./TranslateSubtitle.css";
 
 export default function TranslateSubtitle() {
   const navigate = useNavigate();
+  // const [subtitles, setSubtitles] = useState([]);
+  const [file, setFile] = useState();
   const [textInput, setTextInput] = useState("");
   //   const [comments, setComments] = useState([]);
   const [isTextarea, setIsTextarea] = useState(true);
   //   const [isComments, setIsComments] = useState(false);
+
+  const getFile = (event) => {
+    if (event.target.files[0]) {
+      setFile(event.target.files[0]);
+    }
+  };
+
+  const get_SUb_title = () => {
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = (e) => {
+      const fileContent = e.target.result;
+      const parser = new srtParser2();
+      const srt_array = parser.fromSrt(fileContent);
+      let subtitles = "";
+      for (let index = 0; index < srt_array.length; index++) {
+        subtitles += srt_array[index]["text"] + " ";
+      }
+      setTextInput(subtitles);
+    };
+  };
 
   return (
     <div>
@@ -29,29 +53,30 @@ export default function TranslateSubtitle() {
         <div className="btnGroup">
           <button
             onClick={() => {
-              const a = textInput.split("\n");
-              const b = [];
+              // const a = textInput.split("\n");
+              // const b = [];
 
-              for (let index = 0; index < a.length; index++) {
-                if (a[index].charAt(0) === '"') {
-                  a[index] = a[index].replace('"', "");
-                }
-                if (a[index].endsWith('"')) {
-                  a[index] = a[index].slice(0, -1);
-                }
+              // for (let index = 0; index < a.length; index++) {
+              //   if (a[index].charAt(0) === '"') {
+              //     a[index] = a[index].replace('"', "");
+              //   }
+              //   if (a[index].endsWith('"')) {
+              //     a[index] = a[index].slice(0, -1);
+              //   }
 
-                a[index] = a[index].replace(' "', "");
-                const texts = a[index].split(":");
-                if (texts[1] !== undefined && texts[1].charAt(0) === ' "') {
-                  texts[1] = texts[1].replace(' "', "");
-                }
+              //   a[index] = a[index].replace(' "', "");
+              //   const texts = a[index].split(":");
+              //   if (texts[1] !== undefined && texts[1].charAt(0) === ' "') {
+              //     texts[1] = texts[1].replace(' "', "");
+              //   }
 
-                b.push({ id: index, text1: texts[0], text2: texts[1] });
-              }
+              //   b.push({ id: index, text1: texts[0], text2: texts[1] });
+              // }
 
               //   setComments(b);
-              setIsTextarea(false);
+              // setIsTextarea(false);
               //   setIsComments(true);
+              get_SUb_title();
             }}
           >
             Ok
@@ -66,43 +91,12 @@ export default function TranslateSubtitle() {
             Show Input
           </button>
 
-          <button
-            onClick={async () => {
-              // const { Configuration, OpenAIApi } = require("openai");
-              // const configuration = new Configuration({
-              //   apiKey: "",
-              // });
-              // const openai = new OpenAIApi(configuration);
-              // const response = await openai.createCompletion({
-              //   model: "text-davinci-003",
-              //   prompt:
-              //     "Translate this into 1. French, 2. Spanish and 3. Japanese:\n\nWhat rooms do you have available?\n\n1.",
-              //   temperature: 0.3,
-              //   max_tokens: 100,
-              //   top_p: 1.0,
-              //   frequency_penalty: 0.0,
-              //   presence_penalty: 0.0,
-              // });
-              // setTextInput(response);
-            }}
-          >
-            tool
+          <button onClick={() => navigator.clipboard.writeText(textInput)}>
+            Copy
           </button>
-
           <button onClick={() => navigate("/home")}>Home</button>
+          <input type="file" onChange={getFile} />
         </div>
-
-        {isTextarea ? (
-          <textarea
-            rows={"30"}
-            cols={"60"}
-            placeholder={"Mô tả"}
-            value={textInput}
-            onChange={(e) => {
-              setTextInput(e.target.value);
-            }}
-          ></textarea>
-        ) : null}
       </div>
     </div>
   );
