6f9d314341e94b659cc9f115ecfd0c56d9fd1bd1
muoncode <121119090+muoncode@users.noreply.github.com>
2024-06-05 00:50:38 +0700

    fixx error page get subtitle

--- src/components/GetSubtitle/GetSubtitle.js
+++ src/components/GetSubtitle/GetSubtitle.js
@@ -25,7 +25,7 @@ export default function GetSubtitle() {
   };
 
   const get_SUb_title = () => {
-    if (textInput.length === 0) {
+    if (file) {
       alert("Bạn chưa chọn FILE nha!");
       return;
     }
