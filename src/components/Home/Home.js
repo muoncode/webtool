c5fa782c23b802fc6ca9ebdd7d85130000164d24
muoncode <121119090+muoncode@users.noreply.github.com>
2024-11-23 09:50:56 +0700

    edit css , position of home menu

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,53 +7,57 @@ export default function Home() {
   const navigate = useNavigate();
 
   return (
-    <div className="container_home">
-      <button
-        className="btn_getCmt"
-        onClick={() => navigate("/GetSubtitle")}
-      >
-        Get Subtitle
-      </button>
-      <button className="btn_getCmt" onClick={() => navigate("/getcommment")}>
-        Get Comments
-      </button>
-      <button className="btn_getCmt" onClick={() => navigate("/gettranslate")}>
-        Get Text Translate
-      </button>
+    <div className="container_menu">
+      <div className="container_home">
+        <button
+          className="btn_getCmt"
+          onClick={() => navigate("/GetSubtitle")}
+        >
+          Get Subtitle
+        </button>
+        <button className="btn_getCmt" onClick={() => navigate("/getcommment")}>
+          Get Comments
+        </button>
+        <button className="btn_getCmt" onClick={() => navigate("/gettranslate")}>
+          Get Text Translate
+        </button>
 
-      <button className="btn_getCmt" onClick={() => navigate("/webtool")}>
-        Edit text
-      </button>
-      <button className="btn_getCmt" onClick={() => navigate("/tts_json")}>
-        TTS Json
-      </button>
+        <button className="btn_getCmt" onClick={() => navigate("/webtool")}>
+          Edit text
+        </button>
+        <button className="btn_getCmt" onClick={() => navigate("/tts_json")}>
+          TTS Json
+        </button>
 
-      <button className="btn_getCmt" onClick={() => navigate("/tts_us")}>
-        TTS US
-      </button>
+        <button className="btn_getCmt" onClick={() => navigate("/tts_us")}>
+          TTS US
+        </button>
 
-      <button className="btn_getCmt" onClick={() => navigate("/transfer__money")}>
-      Transfer Money
-      </button>
+        <button className="btn_getCmt" onClick={() => navigate("/transfer__money")}>
+          Transfer Money
+        </button>
 
-      <button className="btn_getCmt" onClick={() => navigate("/webtool1")}>
-        Test
-      </button>
+        <button className="btn_getCmt" onClick={() => navigate("/webtool1")}>
+          Test
+        </button>
 
-      <button className="btn_getCmt" onClick={() => navigate("/testvideo")}>
-        Test Video
-      </button>
+        <button className="btn_getCmt" onClick={() => navigate("/testvideo")}>
+          Test Video
+        </button>
 
-      <button
-        className="btn_getCmt"
-        onClick={() => navigate("/ContinuousColorChange")}
-      >
-        Continuous Color Change
-      </button>
+        <button
+          className="btn_getCmt"
+          onClick={() => navigate("/ContinuousColorChange")}
+        >
+          Continuous Color Change
+        </button>
 
-      <button className="btn_getCmt" onClick={() => navigate("/DownloadImageFromURL")}>
-        Download Image From URL
-      </button>
+        <button className="btn_getCmt" onClick={() => navigate("/DownloadImageFromURL")}>
+          Download Image From URL
+        </button>
+
+      </div>
+
     </div>
   );
 }
