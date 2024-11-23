8b5c9f4a1367b05a9edc21270bcf410bebcfce8c
muoncode <121119090+muoncode@users.noreply.github.com>
2024-11-23 10:04:04 +0700

    test transfer money

--- src/components/UsdToVND/UsdToVND.js
+++ src/components/UsdToVND/UsdToVND.js
@@ -1,4 +1,19 @@
+import { useState } from "react";
+
+import "./UsdToVND.css"
+
 export default function UsdToVND() {
-    return;
+
+    const [usdTOvnd, setUsdTOvnd] = useState('');
+
+    return (
+        <div className="container_transfer_money">
+            <div class="form-group">
+                <label >1 USD = </label>
+                <input type="text" value={usdTOvnd} placeholder="Enter your current USD" onChange={(e) => setUsdTOvnd(e.target.value)} />
+            </div>
+
+        </div>
+    );
 };
 
