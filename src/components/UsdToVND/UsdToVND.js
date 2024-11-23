74f7667066a3948a03fd63960e6df6941249273f
muoncode <121119090+muoncode@users.noreply.github.com>
2024-11-23 10:18:23 +0700

    preview css

--- src/components/UsdToVND/UsdToVND.js
+++ src/components/UsdToVND/UsdToVND.js
@@ -5,12 +5,14 @@ import "./UsdToVND.css"
 export default function UsdToVND() {
 
     const [usdTOvnd, setUsdTOvnd] = useState('');
+    const [vnd, setVnd] = useState('');
+    const [transferMoney, setTransferMoney] = useState('');
 
     return (
         <div className="container_transfer_money_menu">
             <div className="container_transfer_money">
                 <div class="form-group">
-                    <label >1 USD = </label>
+                    <label >1 USD   = </label>
                     <input type="text" value={usdTOvnd} placeholder="Enter your current USD" onChange={(e) => setUsdTOvnd(e.target.value)} />
                 </div>
 
