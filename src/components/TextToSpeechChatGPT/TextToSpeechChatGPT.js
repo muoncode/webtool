0d5b2dab38975dae4bfa9864d8306701c5548f37
muoncode <121119090+muoncode@users.noreply.github.com>
2025-07-22 23:23:19 +0700

    test tts

--- src/components/TextToSpeechChatGPT/TextToSpeechChatGPT.js
+++ src/components/TextToSpeechChatGPT/TextToSpeechChatGPT.js
@@ -20,38 +20,58 @@ function TextToSpeechChatGPT() {
 
     const tts_axios = () => {
 
-        axios.get('https://chatgpt.com/backend-api/synthesize', {
-            params: {
-                message_id: '0c327a58-5825-4d8c-a002-0dc27b164818',
-                conversation_id: '6755b905-fa0c-8012-8784-a24eb7008416',
-                voice: 'juniper',
-                format: 'aac'
+        const apiKey = "AIzaSyBdXcs9-IhKV2wZe4m4MpuQ3IJN-kYQ4Vs";
+        const model = 'gemini-2.5-flash-preview-tts';
+        const api = 'streamGenerateContent';
+        const url = `https://generativelanguage.googleapis.com/v1beta/models/${model}:${api}?key=${apiKey}`;
+
+        const payload = {
+            contents: [
+                {
+                    role: 'user',
+                    parts: [
+                        {
+                            text: 'hello',
+                        },
+                    ],
+                },
+            ],
+            generationConfig: {
+                responseModalities: ['audio'],
+                temperature: 1,
+                speech_config: {
+                    voice_config: {
+                        prebuiltVoiceConfig: {
+                            voice_name: 'Charon',
+                        },
+                    },
+                },
             },
-            headers: {
-                accept: '*/*',
-                'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
-                authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UaEVOVUpHTkVNMVFURTRNMEZCTWpkQ05UZzVNRFUxUlRVd1FVSkRNRU13UmtGRVFrRXpSZyJ9.eyJwd2RfYXV0aF90aW1lIjoxNzMyODk5MjM0NTc5LCJzZXNzaW9uX2lkIjoiZi0tWUZfSUlBYkZmcUpxS3YyQ2pMOVlRRVNuUUplUkIiLCJodHRwczovL2FwaS5vcGVuYWkuY29tL3Byb2ZpbGUiOnsiZW1haWwiOiJpdGFpc3YxOTk5QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlfSwiaHR0cHM6Ly9hcGkub3BlbmFpLmNvbS9hdXRoIjp7InBvaWQiOiJvcmctbkdsMDdxMzVzOG50azZIM1Q2eE1tOWdsIiwidXNlcl9pZCI6InVzZXIteUtmSG5GblhPVjltTWNVVGJwMXJrcEthIn0sImlzcyI6Imh0dHBzOi8vYXV0aDAub3BlbmFpLmNvbS8iLCJzdWIiOiJhdXRoMHw2MzlkZDU3Mjc5NDEyOGFmMmVmMWFhMjkiLCJhdWQiOlsiaHR0cHM6Ly9hcGkub3BlbmFpLmNvbS92MSIsImh0dHBzOi8vb3BlbmFpLm9wZW5haS5hdXRoMGFwcC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNzMyODk5MjM2LCJleHAiOjE3MzM3NjMyMzYsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgbW9kZWwucmVhZCBtb2RlbC5yZXF1ZXN0IG9yZ2FuaXphdGlvbi5yZWFkIG9yZ2FuaXphdGlvbi53cml0ZSBvZmZsaW5lX2FjY2VzcyIsImF6cCI6IlRkSkljYmUxNldvVEh0Tjk1bnl5d2g1RTR5T282SXRHIn0.N0q8nJlS5KtTelWBUqipARE2ql7p8LQAqFw3n3B_bsOB6bN3BIf80sUG8HdbjZfMPhjkBn1VkL47evMX9yrY34nSwT4BKRqPTEm0OkzG4POeyUc1L7YBg-yOA52pqMYIPwJz_vdsMdf75tizqmtZZgvz5eLPSE2NHVPgz0YZMqE1JI87_SPCnPh20OB20FxN14ambz29SDIHtyHT86lJUq_udgzJwrl9NjE3ovKkOOwXy5DvxZsxAnRWh76lG6ST-jvxzy4allNVZAjYyfyRciCCOrWGC-GhdWqltMMcpJZqbCLIeG5VogXAfLQa2MqPyD-ToGd5eKEmG_WqkvgCUA',
-                'oai-device-id': '674763cc-0bf5-4d19-a393-a01785fe9137',
-                'oai-language': 'vi-VN',
-                priority: 'u=1, i',
-                'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
-                'sec-ch-ua-arch': '"x86"',
-                'sec-ch-ua-bitness': '"64"',
-                'sec-ch-ua-full-version': '"131.0.6778.109"',
-                'sec-ch-ua-full-version-list': '"Google Chrome";v="131.0.6778.109", "Chromium";v="131.0.6778.109", "Not_A Brand";v="24.0.0.0"',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-model': '""',
-                'sec-ch-ua-platform': '"Windows"',
-                'sec-ch-ua-platform-version': '"10.0.0"',
-                'sec-fetch-dest': 'empty',
-                'sec-fetch-mode': 'cors',
-                'sec-fetch-site': 'same-origin'
-            }
-        })
+        };
+
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            data: payload,
+        };
+
+
+
+        axios(url, options)
             .then(response => {
-                const res = response.data;
-                console.log(response);
-                
+                // const res = response.data;
+                const chunks = response.data;
+
+
+                for (const chunk of chunks) {
+                    if (chunk.candidates?.[0]?.content?.parts?.[0]?.inlineData) {
+
+                        const { mimeType, data } = chunk.candidates[0].content.parts[0].inlineData;
+                        // const fileUrl = await saveToDrive(fileName, mimeType, data);
+                        setBase64Audio("data:audio/wav;base64," + data);
+                    }
+                }
+
             })
             .catch(error => {
                 if (textInput !== "") {
@@ -59,7 +79,7 @@ function TextToSpeechChatGPT() {
                 }
                 setTextInput(error + "\n\nGặp lỗi nên không thể Chuyển văn bản sang Giọng Nói");
             });
-    // end function tts_axios
+        // end function tts_axios
     };
 
     return (
