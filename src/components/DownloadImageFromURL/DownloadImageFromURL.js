5f692a5ae912a5cf814909aeae9e7f5dce7cf0d0
muoncode <121119090+muoncode@users.noreply.github.com>
2026-02-21 22:21:10 +0700

    test

--- src/components/DownloadImageFromURL/DownloadImageFromURL.js
+++ src/components/DownloadImageFromURL/DownloadImageFromURL.js
@@ -9,6 +9,14 @@ function DownloadImageFromURL() {
 
     const navigate = useNavigate();
     const [textInput, setTextInput] = useState("");
+    const [message, setMessage] = useState("");
+
+        const thong__bao = (text) => {
+        setMessage(text);
+        setTimeout(() => {
+            setMessage("");
+        }, 1000);
+    };
 
     const download_image = async () => {
         if (textInput.length === 0) {
@@ -53,8 +61,17 @@ function DownloadImageFromURL() {
             ></textarea>
 
             <div className="btnGroup">
+                <button onClick={() => {
+                    navigator.clipboard.readText().then((url) => {
+                        if (!url.includes("/reel/")) return url;
+
+  const videoId = url.replace(/\/+$/, "").split("/reel/")[1];
+  navigator.clipboard.writeText(`https://www.facebook.com/watch/?v=${videoId}`);
+                    });
+                }} >Download</button>
                 <button onClick={download_image} >Download</button>
                 <button onClick={() => navigate("/home")}>Home</button>
+                <span className="messageText">{message}</span>
             </div>
         </div>
     );
